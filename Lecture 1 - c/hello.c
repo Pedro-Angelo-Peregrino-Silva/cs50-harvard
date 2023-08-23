54a11c996b171665b1231ed7e3e2a9626a9e92c2
Pedro Ângelo Peregrino e Silva <140512570+Fourx87@users.noreply.github.com>
2023-08-23 20:38:47 -0300

    update hello.c

--- Lecture 1 - c/hello.c
+++ Lecture 1 - c/hello.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
-#include "cs50.h"
+#include <cs50.h>
 
 
 int main(void)
 {
-    
+
     string answer = get_string("Whats is your name? ");
     printf("Hello, %s\n", answer);
 
